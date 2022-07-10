2e39fcbe4c8082174b803bfdc3b4926f86e678c1
Derrick Williamson <dmwilliamson@gmail.com>
2022-07-10 12:42:23 -0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/amateur_radio_quiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amateur_radio_quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AmateurRadioQuiz());
+}
+
+class AmateurRadioQuiz extends StatelessWidget {
+  const AmateurRadioQuiz({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
